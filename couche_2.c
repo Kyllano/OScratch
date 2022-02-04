8b76d4aaf6b862e418be589314a6d3960f4bb102
Kyllano <kyll@ctemplar.com>
2022-02-04 10:21:02 +0100

    update write_super_block

--- couche_2.c
+++ couche_2.c
@@ -2,10 +2,10 @@
 
 
 void write_super_block(){
-    write_block(disk->super_block.number_of_files, 0);
-    write_block(disk->super_block.number_of_users, 1);
-    write_block(disk->super_block.nb_blocks_used , 2);
-    write_block(disk->super_block.first_free_byte, 3);
+    write_block(disk.super_block.number_of_files, 0);
+    write_block(disk.super_block.number_of_users, 1);
+    write_block(disk.super_block.nb_blocks_used , 2);
+    write_block(disk.super_block.first_free_byte, 3);
 }
 
-void read_super
+
