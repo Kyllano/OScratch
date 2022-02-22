4c54895a6f6d9fde652398954311e434fcb5f33f
Victor <oxymore.compositor@gmail.com>
2022-02-22 14:59:10 +0100

    Couche 4 Victor
    Ajout de :
    + int get_file_id(char* filename);
    + int read_file(char* filename, file_t* file);
    + int delete_file(char* filename);
    
    A tester.

--- couche_4.c
+++ couche_4.c
@@ -3,3 +3,39 @@
     \author JUAN Victor, CHEVALLIER Guilhem, ALI-CHERIF Keylan
     \date 8 février 2022
 */
+
+#include "OS_DEFINES.h"
+
+
+int get_file_id(char* filename){
+
+    int i=0;
+    while (!strcmp(disk.inodes[i].filename, filename) && i<disk.super_block.number_of_files) i++;
+
+    if (i >= disk.super_block.number_of_files) return -1;
+    else return i;
+}
+
+
+int read_file(char* filename, file_t* file){
+
+    int i = get_file_id(filename);
+    if (i==-1) return 0;
+    
+    read_mult_blocks(file->data, disk.inodes[i].nblock, disk.inodes[i].first_byte);
+    file->size = disk.inodes[i].size;
+
+    return 1;
+}
+
+
+int delete_file(char* filename){
+    
+    int i = get_file_id(filename);
+    if (i==-1) return 0;
+
+    delete_inode(i);
+    
+    return 1;
+}
+
