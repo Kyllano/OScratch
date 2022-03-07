5a4593d39c0391234a7c93ba8685b03782323929
XenoSwifts15 <guilhem.chevallier15@orange.fr>
2022-03-07 20:04:19 +0100

    ma partie ( c'est loin d'être fini )

--- code/couche_5.c
+++ code/couche_5.c
@@ -22,14 +22,34 @@ int cmd_rm(){
 }
 
 
-int cmd_cr(){
+int cmd_cr(char *filename){
+  int unused_inode;
+  if((unused_inode=get_unused_inode())==-1){
+    printf("il n'y a plus de d'espace disque disponible la création d'un fichier\n");
+    return 1;
+  }
+  init_inode(filename,1, disk.super_block.first_free_byte);
+  strcpy(disk.inodes[unused_inode].ctimestamp, timestamp());
+  strcpy(disk.inodes[unused_inode].mtimestamp, disk.inodes[unused_inode].ctimestamp);
+  disk.inodes[unused_inode].nblock = 1
+  disk.inodes[unused_inode].uid = user.userid;
+  disk.inodes[unused_inode].uright = rw;
+  disk.inodes[unused_inode].oright = rw;
+  update_first_free_byte();
+  disk.super_block.number_of_files ++;
+  return 0;
 
 }
 
 
-int cmd_edit(){
-
+int cmd_edit(char *filename){
+  int i;
+  if((i=get_file_id(filename))==-1){
+    return 1;
+  }
+  if(disk.inodes[i].uid==user.userid && (disk.inodes[i].uright=Rw || disk.inodes[i].uright=RW))
 }
+
 
 
 int cmd_load(){
