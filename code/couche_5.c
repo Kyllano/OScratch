eb3063ec10db29a0d4a6b2b358282a3aea7afdb6
XenoSwifts15 <guilhem.chevallier15@orange.fr>
2022-03-09 19:40:00 +0100

    correction

--- code/couche_5.c
+++ code/couche_5.c
@@ -7,43 +7,8 @@
 #include "OS_DEFINES.h"
 
 // Victor
-int cmd_ls(int type){
-    printf(BOLD WHITE"filename                        "DEF);
+int cmd_ls(){
 
-    // Not Short
-    if (type != 1) {
-        printf(" │ "BOLD WHITE"size    "DEF);
-        printf(" │ "BOLD WHITE"date de création          "DEF);
-    }
-
-    // Long
-    if (type ==2){
-        printf(" │ "BOLD WHITE"date de modification      "DEF);
-        printf(" │ "BOLD WHITE"owner id"DEF);
-    }
-
-    printf("\n");
-
-
-    for (int i=0; i<disk.super_block.number_of_files; i++){
-        printf("%32s │ ", disk.inodes[i].filename);
-
-        // Not Short
-        if (type != 1) {
-            printf(" │ %8d", disk.inodes[i].size);
-            printf(" │ %s", disk.inodes[i].ctimestamp);
-        }
-
-        // Long
-        if (type ==2){
-            printf(" │ %s", disk.inodes[i].mtimestamp);
-            printf(" │ %8d", disk.inodes[i].uid);
-        }
-
-        printf("\n");
-    }
-
-    return NO_ERROR;
 }
 
 // Victor
@@ -51,87 +16,77 @@ int cmd_cat(){
 
 }
 
-// Guilhem
-int cmd_rm(char* filename){
-	int i;
-	if ((i = get_file_id(filename)) == -1){
-		return ERROR_FILE_ACCESS;
-	}
-	if((disk.inodes[i].uid == user.userid && disk.inodes[i].uright != rW && disk.inodes[i].uright != RW)
-	|| (disk.inodes[i].uid != user.userid && disk.inodes[i].oright != rW && disk.inodes[i].oright != RW)){
-		return 2;
-	}
-	return delete_file(filename);
+int cmd_rm(){
+  int i;
+  if((i=get_file_id(filename))==-1){
+    return ERROR_FILE_ACCESS;
+  }
+  if((disk.inodes[i].uid==user.userid && disk.inodes[i].uright!=rW&& disk.inodes[i].uright!=RW)
+  || (disk.inodes[i].uid!=user.userid && disk.inodes[i].oright!=rW && disk.inodes[i].oright!=RW)){
+    return 2;
+  }
+  return delete_file(filename);
 }
 
-// Guilhem
+
 int cmd_cr(char *filename){
-	int unused_inode;
-	
-	if ((unused_inode = get_unused_inode()) == -1){
-		printf(RED"Erreur : Il n'y a plus de d'espace disque disponible la création d'un fichier\n"DEF);
-		return ERROR_DISK_FULL;
-	}
+  int unused_inode;
+  if((unused_inode=get_unused_inode())==-1){
+    printf("il n'y a plus de d'espace disque disponible la création d'un fichier\n");
+    return 1;
+  }
+  init_inode(filename,1, disk.super_block.first_free_byte);
+  strcpy(disk.inodes[unused_inode].ctimestamp, timestamp());
+  strcpy(disk.inodes[unused_inode].mtimestamp, disk.inodes[unused_inode].ctimestamp);
+  disk.inodes[unused_inode].nblock = 1;
+  disk.inodes[unused_inode].uid = user.userid;
+  disk.inodes[unused_inode].uright = rw;
+  disk.inodes[unused_inode].oright = rw;
+  update_first_free_byte();
+  disk.super_block.number_of_files ++;
+  return 0;
 
-	init_inode(filename, 1, disk.super_block.first_free_byte);
-	
-	strcpy(disk.inodes[unused_inode].ctimestamp, timestamp());
-	strcpy(disk.inodes[unused_inode].mtimestamp, disk.inodes[unused_inode].ctimestamp);
-	disk.inodes[unused_inode].nblock = 1;
-	disk.inodes[unused_inode].uid = user.userid;
-	disk.inodes[unused_inode].uright = rw;
-	disk.inodes[unused_inode].oright = rw;
-	
-	update_first_free_byte();
-	disk.super_block.number_of_files ++;
-	
-	return NO_ERROR;
 }
 
-// Guilhem
+
 int cmd_edit(char *filename){
-	
-	int i;
-	file_t file;
-	char c;
-	
-	if ((i = get_file_id(filename)) == -1){
-		return ERROR_FILE_ACCESS;
-	}
+  int i;file_t file;char c;
+  if((i=get_file_id(filename))==-1){
+    return ERROR_FILE_ACCESS;
+  }
+  if((disk.inodes[i].uid==user.userid && disk.inodes[i].uright!=Rw && disk.inodes[i].uright!=RW)
+  || (disk.inodes[i].uid!=user.userid && disk.inodes[i].oright!=Rw && disk.inodes[i].oright!=RW)){
+    return 2;
+  }
+  printf("entrez le contenu du fichier (terminer la saisie avec une tabulation)\n");
+  file.size=0;
+  while((c=fgetc(stdin))!='\t' && file.size!=MAX_FILE_SIZE){
+    file.data[file.size++]=c;
+  }
+  return write_file(filename,&file);
 
-	if ((disk.inodes[i].uid == user.userid && disk.inodes[i].uright != Rw && disk.inodes[i].uright != RW)
-	|| (disk.inodes[i].uid != user.userid && disk.inodes[i].oright != Rw && disk.inodes[i].oright != RW)){
-		return ERROR_RIGHTS;
-	}
 
-	printf("entrez le contenu du fichier (terminer la saisie avec une tabulation)\n");
-	
-	file.size = 0;
-	while ((c = fgetc(stdin)) != '\t' && file.size != MAX_FILE_SIZE){
-		file.data[file.size++] = c;
-	}
-
-	return write_file(filename, &file);
 }
 
-// Guilhem
+
+
 int cmd_load(char *filename){
-	file_t *file;
-	return load_file_from_host(filename, file);
+  file_t file;
+  return load_file_from_host(filename,&file);
 
 }
 
-// Guilhem
+
 int cmd_store(char *filename){
-  	return store_file_to_host(filename);
+  return store_file_to_host(filename);
 }
 
-// Keylan
+
 int cmd_chown(){
 
 }
 
-// KEylan
+
 int cmd_chmod(){
 
 }
