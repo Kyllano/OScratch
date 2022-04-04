ca73046df08461558d34df794b2246bb80601bcb
Oxy <oxymore.compositor@gmail.com>
2022-04-04 18:04:41 +0200

    Boutons fonctionnels mais fenêtre ne ferme pas
    je sais pas comment faire là

--- Java/src/GUI/GUIMain.java
+++ Java/src/GUI/GUIMain.java
@@ -2,44 +2,78 @@ package GUI;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
 
 /* Help from :
 - https://www.guru99.com/java-swing-gui.html
 - https://stackoverflow.com/questions/2536873/how-can-i-set-size-of-a-button
-
-
+- https://docs.oracle.com/javase/tutorial/uiswing/events/actionlistener.html
+- https://stackoverflow.com/questions/5911565/how-to-add-multiple-actionlisteners-for-multiple-buttons-in-java-swing
 */
 
-public class GUIMain {
+public class GUIMain extends Frame{
+	JPanel buttonsPanel;
+	JButton butDefrag;
+	JButton butVerif;
+	JLabel outputLabel;
+	
 	public static void main(String args[]){
+		GUIMain window = new GUIMain("OSCratch management tool");
+		//window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setSize(415,200);
 		
-		// Creating the frame
-		JFrame frame = new JFrame("OSCratch management tool");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(415,200);
+		window.setVisible(true);
+    }
+
+	public GUIMain (String title) {
+		super(title);
 		
+		setLayout(new FlowLayout());		
 		
 		// Creating the main panel
-		JPanel buttonsPanel = new JPanel();
-		   
-		JButton button1 = new JButton("Défragmentation");
-		button1.setPreferredSize(new Dimension(200, 100));
-		JButton button2 = new JButton("Vérification");
-		button2.setPreferredSize(new Dimension(200, 100));
+		buttonsPanel = new JPanel();
+
+		butDefrag = new JButton("Défragmentation");
+		butDefrag.setPreferredSize(new Dimension(200, 100));
+		butDefrag.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				System.out.println("Défragmentation");
+				outputLabel.setText("Défragmentation");
+			}
+		});
 		
-		buttonsPanel.add(button1);
-		buttonsPanel.add(button2);
+		butVerif = new JButton("Vérification");
+		butVerif.setPreferredSize(new Dimension(200, 100));
+		butVerif.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				System.out.println("Vérification");
+				outputLabel.setText("Vérification");
+			}
+		});
 		
+		buttonsPanel.add(butDefrag);
+		buttonsPanel.add(butVerif);
+
 		
 		//Creating an output label
-		JLabel outputLabel = new JLabel("");
+		outputLabel = new JLabel("");
 		
 		
 		// Adding components to the frame
-		frame.getContentPane().add(BorderLayout.CENTER, buttonsPanel);
-		frame.getContentPane().add(BorderLayout.SOUTH, outputLabel);
-		
-		frame.setVisible(true);
-    }
+		add(BorderLayout.CENTER, buttonsPanel);
+		add(BorderLayout.SOUTH, outputLabel);
+	}
+	
+	
+	public void windowClosing(WindowEvent e) {
+	        dispose();
+	        System.exit(0);
+	}
+	public void windowOpened(WindowEvent e) {}
+    public void windowActivated(WindowEvent e) {}
+    public void windowIconified(WindowEvent e) {}
+    public void windowDeiconified(WindowEvent e) {}
+    public void windowDeactivated(WindowEvent e) {}
+    public void windowClosed(WindowEvent e) {}
 }
