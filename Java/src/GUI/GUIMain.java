ef8a63eeb665245a29d02f6c26c815fe3c4a5058
Oxy <oxymore.compositor@gmail.com>
2022-04-05 14:42:27 +0200

    GUI prêt à accueillir le code

--- Java/src/GUI/GUIMain.java
+++ Java/src/GUI/GUIMain.java
@@ -10,9 +10,10 @@ import java.awt.event.*;
 - https://stackoverflow.com/questions/2536873/how-can-i-set-size-of-a-button
 - https://docs.oracle.com/javase/tutorial/uiswing/events/actionlistener.html
 - https://stackoverflow.com/questions/5911565/how-to-add-multiple-actionlisteners-for-multiple-buttons-in-java-swing
+- https://stackoverflow.com/questions/34250434/jframe-setdefaultcloseoperation-not-working
 */
 
-public class GUIMain extends Frame{
+public class GUIMain extends JFrame{
 	JPanel buttonsPanel;
 	JButton butDefrag;
 	JButton butVerif;
@@ -20,7 +21,7 @@ public class GUIMain extends Frame{
 	
 	public static void main(String args[]){
 		GUIMain window = new GUIMain("OSCratch management tool");
-		//window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setSize(415,200);
 		
 		window.setVisible(true);
@@ -38,7 +39,12 @@ public class GUIMain extends Frame{
 		butDefrag.setPreferredSize(new Dimension(200, 100));
 		butDefrag.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				
+				// Code défragmentation
 				System.out.println("Défragmentation");
+				
+				
+				// Retour dans outputLabel
 				outputLabel.setText("Défragmentation");
 			}
 		});
@@ -47,7 +53,11 @@ public class GUIMain extends Frame{
 		butVerif.setPreferredSize(new Dimension(200, 100));
 		butVerif.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				
+				// Code vérification
 				System.out.println("Vérification");
+				
+				// Retour dans outputLabel
 				outputLabel.setText("Vérification");
 			}
 		});
